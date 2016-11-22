4195684aa4778a05f54e9845a6b5f86ebc15c43e
Frank Stein <dr.frank.stain@gmail.com>
2016-11-22 23:07:17 +0200

    Initial commit. First files.

---
+++ jnipp-test/sources/src/com/pfs/jnipptest/MainActivity.java
@@ -0,0 +1,12 @@
+package com.pfs.jnipptest;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
