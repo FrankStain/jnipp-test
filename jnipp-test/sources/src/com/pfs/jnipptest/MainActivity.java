ec0f500fb02edc70002801539563f81f02f57249
Frank Stein <dr.frank.stain@gmail.com>
2016-11-27 00:41:34 +0200

    Interface for tests.

--- jnipp-test/sources/src/com/pfs/jnipptest/MainActivity.java
+++ jnipp-test/sources/src/com/pfs/jnipptest/MainActivity.java
@@ -3,11 +3,14 @@ package com.pfs.jnipptest;
 import android.os.Bundle;
 import android.app.Activity;
 
+import nothing.R;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView( R.layout.main_layout );
     }
 
     static {
