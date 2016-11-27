431fb386a102fc0cce79b34637672b127db8e81d
Frank Stein <dr.frank.stain@gmail.com>
2016-11-27 23:19:35 +0200

    GoogleTests included into build.

--- jnipp-test/sources/jni/Android.mk
+++ jnipp-test/sources/jni/Android.mk
@@ -18,7 +18,7 @@ LOCAL_C_INCLUDES		:= $(LOCAL_PATH)/include
 LOCAL_PATH				:= $(LOCAL_PATH)/source
 LOCAL_MODULE			:= jnipp-test
 LOCAL_SRC_FILES			:= $(patsubst $(LOCAL_PATH)/%, %, $(filter %.cpp, $(call lookup-files, $(LOCAL_PATH))))
-LOCAL_STATIC_LIBRARIES	:= jnipp
+LOCAL_STATIC_LIBRARIES	:= jnipp gtest
 
 
 # Build rules for Android native applications.
@@ -27,3 +27,4 @@ include $(BUILD_SHARED_LIBRARY)
 
 # Include rules for building BitLight libraries.
 $(call import-module, jnipp)
+$(call import-module, gtest)
---
+++ submodules/gtest/Android.mk
@@ -0,0 +1,28 @@
+# Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
+# Apache 2.0 License
+
+
+# Init the building section for project.
+PROJECT_PATH	:= $(call my-dir)/../googletest/googletest
+LOCAL_PATH		:= $(PROJECT_PATH)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE			:= gtest
+LOCAL_EXPORT_C_INCLUDES	:= $(PROJECT_PATH)/include
+LOCAL_C_INCLUDES		:= $(LOCAL_EXPORT_C_INCLUDES)
+
+LOCAL_SRC_FILES			:=	\
+	src/gtest.cc			\
+	src/gtest-death-test.cc	\
+	src/gtest-filepath.cc	\
+	src/gtest-port.cc		\
+	src/gtest-printers.cc	\
+	src/gtest-test-part.cc	\
+	src/gtest-typed-test.cc	\
+
+LOCAL_CPP_FEATURES		:= rtti exceptions
+LOCAL_EXPORT_LDLIBS		:= -latomic -llog
+LOCAL_CPPFLAGS			:= -std=c++11
+
+# Build rules for Android native applications.
+include $(BUILD_STATIC_LIBRARY)
