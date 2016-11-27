1ee6a1b692ec2c015f6977ed90aa8e96d072d979
Frank Stein <dr.frank.stain@gmail.com>
2016-11-27 23:24:21 +0200

    Copyright fixed.

--- jnipp-test/sources/jni/include/main.h
+++ jnipp-test/sources/jni/include/main.h
@@ -1,4 +1,4 @@
-// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
+// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp-test)
 // Apache 2.0 License
 #pragma once
 
