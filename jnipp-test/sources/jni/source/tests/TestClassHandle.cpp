d5b03fd1fe4e9273e4b1db1f76f23dfe852b62db
Frank Stein <dr.frank.stain@gmail.com>
2016-12-06 23:20:44 +0200

    Tests for handle to class.

--- jnipp-test/sources/jni/source/tests/TestClassHandle.cpp
+++ jnipp-test/sources/jni/source/tests/TestClassHandle.cpp
@@ -4,3 +4,78 @@
 #include <gtest/gtest.h>
 
 
+TEST( TestClassHandle, EmptyIsValid )
+{
+	jnipp::ClassHandle class_handle;
+
+	EXPECT_FALSE( class_handle.IsValid() );
+};
+
+TEST( TestClassHandle, ConstructedIsValid )
+{
+	jnipp::ClassHandle class_handle{ "java/lang/String" };
+
+	EXPECT_TRUE( class_handle.IsValid() );
+};
+
+TEST( TestClassHandle, Invalidate )
+{
+	jnipp::ClassHandle class_handle{ "java/lang/String" };
+
+	EXPECT_TRUE( class_handle.IsValid() );
+	
+	class_handle.Invalidate();
+	EXPECT_FALSE( class_handle.IsValid() );
+};
+
+TEST( TestClassHandle, AssignString )
+{
+	jnipp::ClassHandle class_handle;
+
+	EXPECT_FALSE( class_handle.IsValid() );
+
+	class_handle = "java/lang/String";
+	EXPECT_TRUE( class_handle.IsValid() );
+};
+
+TEST( TestClassHandle, CopyValid )
+{
+	jnipp::ClassHandle class_handle1;
+	const jnipp::ClassHandle class_handle2{ "java/lang/String" };
+
+	EXPECT_FALSE( class_handle1.IsValid() );
+	EXPECT_TRUE( class_handle2.IsValid() );
+
+	class_handle1 = class_handle2;
+	EXPECT_TRUE( class_handle1.IsValid() );
+	EXPECT_TRUE( class_handle2.IsValid() );
+
+	const jnipp::ClassHandle class_handle3{ class_handle2 };
+	EXPECT_TRUE( class_handle3.IsValid() );
+	EXPECT_TRUE( class_handle2.IsValid() );
+};
+
+TEST( TestClassHandle, MoveValid )
+{
+	jnipp::ClassHandle class_handle1;
+	jnipp::ClassHandle class_handle2{ "java/lang/String" };
+
+	EXPECT_FALSE( class_handle1.IsValid() );
+	EXPECT_TRUE( class_handle2.IsValid() );
+
+	class_handle1 = std::move( class_handle2 );
+	EXPECT_TRUE( class_handle1.IsValid() );
+	EXPECT_FALSE( class_handle2.IsValid() );
+
+	jnipp::ClassHandle class_handle3{ std::move( class_handle1 ) };
+	EXPECT_TRUE( class_handle3.IsValid() );
+	EXPECT_FALSE( class_handle1.IsValid() );
+};
+
+TEST( TestClassHandle, GetReference )
+{
+	jnipp::ClassHandle class_handle{ "java/lang/String" };
+
+	EXPECT_TRUE( class_handle.IsValid() );
+	EXPECT_NE( nullptr, *class_handle );
+};
