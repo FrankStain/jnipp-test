3af3c7b6b8302b43b315a66120f12aa2c9f69ac2
Frank Stein <dr.frank.stain@gmail.com>
2016-12-10 20:48:16 +0200

    Native function tests updated.

--- jnipp-test/sources/jni/source/tests/TestNativeFunction.cpp
+++ jnipp-test/sources/jni/source/tests/TestNativeFunction.cpp
@@ -23,65 +23,65 @@ jnipp::NativeFunction func{																\
 TEST( TestNativeFunction, SignatureVoidNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( void );
-	EXPECT_STREQ( "()V", func.signature.c_str() );
+	EXPECT_STREQ( "()V", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureBooleanNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jboolean );
-	EXPECT_STREQ( "()Z", func.signature.c_str() );
+	EXPECT_STREQ( "()Z", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureStringNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jstring );
-	EXPECT_STREQ( "()Ljava/lang/String;", func.signature.c_str() );
+	EXPECT_STREQ( "()Ljava/lang/String;", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureFloatNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jfloat );
-	EXPECT_STREQ( "()F", func.signature.c_str() );
+	EXPECT_STREQ( "()F", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureDoubleNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jdouble );
-	EXPECT_STREQ( "()D", func.signature.c_str() );
+	EXPECT_STREQ( "()D", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureByteNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jbyte );
-	EXPECT_STREQ( "()B", func.signature.c_str() );
+	EXPECT_STREQ( "()B", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureCharNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jchar );
-	EXPECT_STREQ( "()C", func.signature.c_str() );
+	EXPECT_STREQ( "()C", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureShortNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jshort );
-	EXPECT_STREQ( "()S", func.signature.c_str() );
+	EXPECT_STREQ( "()S", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureIntNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jint );
-	EXPECT_STREQ( "()I", func.signature.c_str() );
+	EXPECT_STREQ( "()I", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureLongNoArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( jlong );
-	EXPECT_STREQ( "()J", func.signature.c_str() );
+	EXPECT_STREQ( "()J", func.signature );
 };
 
 TEST( TestNativeFunction, SignatureVoidWithArgs )
 {
 	DECLARE_TEST_MEMBER_FUNC( void, jint, jstring, jlong );
-	EXPECT_STREQ( "(ILjava/lang/String;J)V", func.signature.c_str() );
+	EXPECT_STREQ( "(ILjava/lang/String;J)V", func.signature );
 };
