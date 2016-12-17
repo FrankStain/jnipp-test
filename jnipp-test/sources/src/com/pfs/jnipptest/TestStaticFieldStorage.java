f0e93d06e26a102d0a6d7338a378c3bf539ebd90
Frank Stein <dr.frank.stain@gmail.com>
2016-12-17 16:20:09 +0200

    Test class for testing the static field handle.

---
+++ jnipp-test/sources/src/com/pfs/jnipptest/TestStaticFieldStorage.java
@@ -0,0 +1,17 @@
+// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp-test)
+// Apache 2.0 License
+package com.pfs.jnipptest;
+
+
+final class TestStaticFieldStorage
+{
+	private static boolean	bool_field		= true;
+	private static String	string_field	= "Jni++";
+	private static float	float_field		= 13.0f;
+	private static double	double_field	= 42.0;
+	private static byte		byte_field		= 28;
+	private static char		char_field		= 'S';
+	private static short	short_field		= 24;
+	private static int		int_field		= 65536;
+	private static long		long_field		= 1L << 48;
+}
