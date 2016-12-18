469e401d5e23f813c68e18306723452fae65d0bc
Frank Stein <dr.frank.stain@gmail.com>
2016-12-18 13:05:13 +0200

    Tests for validity of function handles.

--- jnipp-test/sources/jni/source/tests/TestFunctionHandle.IsValid.cpp
+++ jnipp-test/sources/jni/source/tests/TestFunctionHandle.IsValid.cpp
@@ -4,3 +4,72 @@
 #include <gtest/gtest.h>
 
 
+#define DECLARE_TEST_ENV( NAME, RET, ... )											\
+	jnipp::ClassHandle class_handle{ "com/pfs/jnipptest/TestFunctionContainer" };	\
+																					\
+	jnipp::FunctionHandle<RET, ##__VA_ARGS__>	func{ class_handle, NAME };
+
+
+TEST( TestFunctionHandle, ValidVoidNoArguments )
+{
+	DECLARE_TEST_ENV( "VoidNoArguments", void );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestFunctionHandle, ValidVoidOneArgument )
+{
+	DECLARE_TEST_ENV( "VoidOneArgument", void, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestFunctionHandle, ValidVoidTwoArguments )
+{
+	DECLARE_TEST_ENV( "VoidTwoArguments", void, std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestFunctionHandle, ValidVoidThreeArguments )
+{
+	DECLARE_TEST_ENV( "VoidThreeArguments", void, std::string, std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestFunctionHandle, ValidStringNoArguments )
+{
+	DECLARE_TEST_ENV( "StringNoArguments", std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestFunctionHandle, ValidStringOneArgument )
+{
+	DECLARE_TEST_ENV( "StringOneArgument", std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestFunctionHandle, ValidStringTwoArguments )
+{
+	DECLARE_TEST_ENV( "StringTwoArguments", std::string, std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestFunctionHandle, ValidStringThreeArguments )
+{
+	DECLARE_TEST_ENV( "StringThreeArguments", std::string, std::string, std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
