7ee9ad48083d1f217f4b8fb44f0b9129406f7113
Frank Stein <dr.frank.stain@gmail.com>
2016-12-19 21:40:30 +0200

    Static function handle validation tests.

--- jnipp-test/sources/jni/source/tests/TestStaticFunctionHandle.IsValid.cpp
+++ jnipp-test/sources/jni/source/tests/TestStaticFunctionHandle.IsValid.cpp
@@ -4,3 +4,72 @@
 #include <gtest/gtest.h>
 
 
+#define DECLARE_TEST_ENV( NAME, RET, ... )												\
+	jnipp::ClassHandle class_handle{ "com/pfs/jnipptest/TestStaticFunctionContainer" };	\
+																						\
+	jnipp::StaticFunctionHandle<RET, ##__VA_ARGS__>	func{ class_handle, NAME };
+
+
+TEST( TestStaticFunctionHandle, ValidVoidNoArguments )
+{
+	DECLARE_TEST_ENV( "VoidNoArguments", void );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestStaticFunctionHandle, ValidVoidOneArgument )
+{
+	DECLARE_TEST_ENV( "VoidOneArgument", void, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestStaticFunctionHandle, ValidVoidTwoArguments )
+{
+	DECLARE_TEST_ENV( "VoidTwoArguments", void, std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestStaticFunctionHandle, ValidVoidThreeArguments )
+{
+	DECLARE_TEST_ENV( "VoidThreeArguments", void, std::string, std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestStaticFunctionHandle, ValidStringNoArguments )
+{
+	DECLARE_TEST_ENV( "StringNoArguments", std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestStaticFunctionHandle, ValidStringOneArgument )
+{
+	DECLARE_TEST_ENV( "StringOneArgument", std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestStaticFunctionHandle, ValidStringTwoArguments )
+{
+	DECLARE_TEST_ENV( "StringTwoArguments", std::string, std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
+
+TEST( TestStaticFunctionHandle, ValidStringThreeArguments )
+{
+	DECLARE_TEST_ENV( "StringThreeArguments", std::string, std::string, std::string, std::string );
+
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_NE( nullptr, *func );
+};
