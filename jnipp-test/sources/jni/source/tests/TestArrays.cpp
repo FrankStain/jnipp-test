6659ccc9526ea4920257e23a44bad896d1b548b0
Frank Stein <dr.frank.stain@gmail.com>
2017-01-03 12:46:22 +0200

    Tests for function calls with arrays.

--- jnipp-test/sources/jni/source/tests/TestArrays.cpp
+++ jnipp-test/sources/jni/source/tests/TestArrays.cpp
@@ -19,8 +19,8 @@ TEST( TestArays, ReadPlainArrayField )
 {
 	Jni::Class class_handle{ "com/pfs/jnipptest/TestArrayStorage" };
 
-	Jni::MemberField<std::vector<bool>>	field{ class_handle, "m_bool_out_array" };
-	Jni::Object							test_object{ Jni::Object::NewObject( class_handle ) };
+	Jni::MemberField<std::vector<bool>>					field{ class_handle, "m_bool_out_array" };
+	Jni::Object											test_object{ Jni::Object::NewObject( class_handle ) };
 
 	EXPECT_TRUE( field.IsValid() );
 	EXPECT_TRUE( test_object.IsValid() );
@@ -46,8 +46,8 @@ TEST( TestArays, WritePlainArrayField )
 {
 	Jni::Class class_handle{ "com/pfs/jnipptest/TestArrayStorage" };
 
-	Jni::MemberField<std::vector<bool>>	field{ class_handle, "m_bool_in_array" };
-	Jni::Object							test_object{ Jni::Object::NewObject( class_handle ) };
+	Jni::MemberField<std::vector<bool>>					field{ class_handle, "m_bool_in_array" };
+	Jni::Object											test_object{ Jni::Object::NewObject( class_handle ) };
 
 	EXPECT_TRUE( field.IsValid() );
 	EXPECT_TRUE( test_object.IsValid() );
@@ -75,8 +75,8 @@ TEST( TestArays, ReadObjectArrayField )
 {
 	Jni::Class class_handle{ "com/pfs/jnipptest/TestArrayStorage" };
 
-	Jni::MemberField<std::vector<std::string>>	field{ class_handle, "m_string_out_array" };
-	Jni::Object		test_object{ Jni::Object::NewObject( class_handle ) };
+	Jni::MemberField<std::vector<std::string>>			field{ class_handle, "m_string_out_array" };
+	Jni::Object											test_object{ Jni::Object::NewObject( class_handle ) };
 
 	EXPECT_TRUE( field.IsValid() );
 	EXPECT_TRUE( test_object.IsValid() );
@@ -99,8 +99,8 @@ TEST( TestArays, WriteObjectArrayField )
 {
 	Jni::Class class_handle{ "com/pfs/jnipptest/TestArrayStorage" };
 
-	Jni::MemberField<std::vector<std::string>>	field{ class_handle, "m_string_in_array" };
-	Jni::Object		test_object{ Jni::Object::NewObject( class_handle ) };
+	Jni::MemberField<std::vector<std::string>>			field{ class_handle, "m_string_in_array" };
+	Jni::Object											test_object{ Jni::Object::NewObject( class_handle ) };
 
 	EXPECT_TRUE( field.IsValid() );
 	EXPECT_TRUE( test_object.IsValid() );
@@ -113,6 +113,61 @@ TEST( TestArays, WriteObjectArrayField )
 
 	std::vector<std::string> values;
 	EXPECT_TRUE( field.GetValue( test_object, values ) );
+
+	EXPECT_EQ( basic_values.size(), values.size() );
+	for( size_t index = 0; index < values.size(); ++index )
+	{
+		EXPECT_EQ( basic_values[ index ], values[ index ] );
+	};
+};
+
+TEST( TestArays, CallFunctionWithArrayArgument )
+{
+	Jni::Class class_handle{ "com/pfs/jnipptest/TestArrayStorage" };
+
+	Jni::MemberField<std::vector<std::string>>			field{ class_handle, "m_string_in_array" };
+	Jni::MemberFunction<void, std::vector<std::string>>	func{ class_handle, "SetStringArray" };
+	Jni::Object											test_object{ Jni::Object::NewObject( class_handle ) };
+
+	EXPECT_TRUE( field.IsValid() );
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_TRUE( test_object.IsValid() );
+
+	const std::vector<std::string> basic_values{
+		{ "Hello" }, { "Jni++" }, { "!!" },
+	};
+
+	func.Call( test_object, basic_values );
+
+	std::vector<std::string> values;
+	EXPECT_TRUE( field.GetValue( test_object, values ) );
+
+	EXPECT_EQ( basic_values.size(), values.size() );
+	for( size_t index = 0; index < values.size(); ++index )
+	{
+		EXPECT_EQ( basic_values[ index ], values[ index ] );
+	};
+};
+
+TEST( TestArays, CallFunctionWithArrayResult )
+{
+	Jni::Class class_handle{ "com/pfs/jnipptest/TestArrayStorage" };
+
+	Jni::MemberField<std::vector<std::string>>			field{ class_handle, "m_string_in_array" };
+	Jni::MemberFunction<std::vector<std::string>>		func{ class_handle, "GetStringArray" };
+	Jni::Object											test_object{ Jni::Object::NewObject( class_handle ) };
+
+	EXPECT_TRUE( field.IsValid() );
+	EXPECT_TRUE( func.IsValid() );
+	EXPECT_TRUE( test_object.IsValid() );
+
+	const std::vector<std::string> basic_values{
+		{ "Hello" }, { "Jni++" }, { "!!" },
+	};
+
+	EXPECT_TRUE( field.SetValue( test_object, basic_values ) );
+
+	std::vector<std::string> values{ func.Call( test_object ) };
 
 	EXPECT_EQ( basic_values.size(), values.size() );
 	for( size_t index = 0; index < values.size(); ++index )
